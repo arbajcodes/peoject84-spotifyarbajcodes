0bd33fc46e0c5c21cf5362359146cb211e6cafe2
arbajcodes <khanarbaj24474@gmail.com>
2025-09-26 12:11:06 +0530

    new changing and add songs

--- js/sc.js
+++ js/sc.js
@@ -25,7 +25,7 @@ function formatSeconds(totalSeconds) {
 
 async function getSongs(folder) {
     currFolder = folder
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -82,7 +82,7 @@ const playMusic = (track, pause = false) => {
 
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:5500/songs/`)
+    let a = await fetch(`/songs/`)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -98,7 +98,7 @@ async function displayAlbums() {
             console.log("hhh" + folder);
 
             //get metadata of the folder
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json()
             console.log(response);
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder = "${folder}" class="card">
